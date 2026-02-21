e113b41d7d5efac9f884c3f48b00820cc61a59a7
Praveen Saraogi <saraogipraveenuk@gmail.com>
2026-02-21 17:58:28 +0000

    style: swap About and Contact component positions in grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,14 @@ export default function Home() {
           <Hero />
         </div>
 
-        {/* Top Right: Contact card */}
-        <div className="lg:col-span-4 lg:row-span-1 flex min-h-[250px]">
-          <Contact />
-        </div>
-
-        {/* Middle Right: Quick Facts/Principles */}
+        {/* Top Right: Quick Facts/Principles */}
         <div className="lg:col-span-4 lg:row-span-2 flex">
           <AboutPrinciples />
+        </div>
+
+        {/* Middle Right: Contact card */}
+        <div className="lg:col-span-4 lg:row-span-1 flex min-h-[250px]">
+          <Contact />
         </div>
 
         {/* Tech Stack / Skills under Hero */}
